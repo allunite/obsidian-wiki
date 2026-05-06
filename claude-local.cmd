34f4a3b93aa683bb3d27465d91eef0a71a801bfb
aka <aka@allunite.com>
2026-05-06 18:30:38 +0300

    claude local cmd

---
+++ claude-local.cmd
@@ -0,0 +1,7 @@
+@echo off
+set ANTHROPIC_AUTH_TOKEN=lmstudio
+set ANTHROPIC_BASE_URL=http://127.0.0.1:1234
+set ANTHROPIC_DEFAULT_OPUS_MODEL=google/gemma-4-e4b
+set ANTHROPIC_DEFAULT_SONNET_MODEL=google/gemma-4-e2b
+set ANTHROPIC_DEFAULT_HAIKU_MODEL=nvidia/nemotron-3-nano-4b
+claude.exe
